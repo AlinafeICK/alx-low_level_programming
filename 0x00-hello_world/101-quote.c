05b4832c866899e7dfd0aeccbd475318bf9eb8f9
AlinafeICK <alinafeick@gmail.com>
2023-08-20 10:36:19 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: a Program that prints a string with
+ * special characters.
+ *
+ * Return: a value of 1
+ *
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
