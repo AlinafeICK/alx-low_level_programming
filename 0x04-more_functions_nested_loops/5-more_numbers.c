68be85b28411f72bc1e2898494b016f5d2fc6191
AlinafeICK <alinafeick@gmail.com>
2023-08-28 16:49:41 -0700

    a function that prints 10 times the numbers, from 0 to 14, followed by a new line.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,14 @@ void more_numbers(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 <=14; num2++)
+		for (num2 = 0; num2 <= 14; num2++)
 		{
-			if(num2 > 9)
+			if (num2 > 9)
 			{
-		_putchar((num2 / 10) + '0');
+				_putchar((num2 / 10) + '0');
 			}
 			_putchar((num2 % 10) + '0');
-	}
-	_putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
