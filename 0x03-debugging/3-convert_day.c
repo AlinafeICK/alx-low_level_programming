f9ec47d550b702919f3b71828292a265d5942b6c
AlinafeICK <alinafeick@gmail.com>
2023-08-27 18:54:38 -0700

    Leap year fix

---
+++ 0x03-debugging/3-convert_day.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month is number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+int convert_day(int month, int day)
+{
+	switch (month)
+		
