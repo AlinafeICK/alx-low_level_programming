28c8a976a5e594988ce0890cd6158f7a85480513
AlinafeICK <alinafeick@gmail.com>
2023-10-12 16:31:10 -0700

    a program that prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-int _putchar(char c);
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1, on error -1 is returned
- * and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
 /**
  * main - program to print the file name its
  * compiled from
@@ -24,19 +9,6 @@ int _putchar(char c)
 
 int main(void)
 {
-	const char *filename = __FILE__;
-	size_t len = 0;
-	size_t i;
-
-	while (filename[len] != '\0')
-	{
-	len++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
-	_putchar(filename[i]);
-	}
-	_putchar('\n');
-	exit(0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
