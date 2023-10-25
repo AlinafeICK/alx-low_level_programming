f785aa0a08761bc67fe82e77141bef855e002ac9
AlinafeICK <alinafeick@gmail.com>
2023-10-24 17:07:26 -0700

    a function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * str_concat - a function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1, len2;
+	char *concatenated;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	concatenated = (char *)malloc((len1 +len2 + 1) * sizeof(char));
+
+	if (concatenated == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(concatenated, s1);
+	strcat(concatenated, s2);
+
+	return (concatenated);
+}
