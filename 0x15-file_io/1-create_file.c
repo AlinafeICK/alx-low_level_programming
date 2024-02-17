4934c7f6fd0c59411af60d224df15ad8b1736251
AlinafeICK <alinafeick@gmail.com>
2024-02-17 03:53:09 +0200

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* create_file - creates file and writes to it
+* @filename: file to create
+* @text_content: NULL terminated string to write to file
+*
+* Return: 1 Success, -1 failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
